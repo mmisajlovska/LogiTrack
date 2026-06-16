ebb7a5fd81539f8952364da0825be65b88180071
mmisajlovska <mmisajlovska011@gmail.com>
2026-06-16 20:24:34 +0200

    updated settings

--- warehouse_project/settings.py
+++ warehouse_project/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,logitrack-django.onrender.com').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,logitrack-django.onrender.com').split(',')
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
