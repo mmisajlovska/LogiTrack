d7a4d7738942dea5cab346c41f95f8ff6c50cdbd
mmisajlovska <mmisajlovska011@gmail.com>
2026-06-30 12:36:20 +0200

    port modified

--- warehouse_project/settings.py
+++ warehouse_project/settings.py
@@ -134,4 +134,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
